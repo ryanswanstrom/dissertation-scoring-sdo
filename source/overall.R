839be0a125da38ce268e11d5ea48050768723624
Ryan Swanstrom <ryanswanstrom@users.noreply.github.com>
2015-03-18 15:31:12 -0500

    some more modifications and include CRI element scatterplot matrix

--- source/overall.R
+++ source/overall.R
@@ -2,9 +2,13 @@
 ### Combine the scores
 # overall
 
-full = merge(x = qual_scores, y = avail_scores, by = "MONTH_DT", all = TRUE)
-full = merge(x = full, y = req_scores, by = "MONTH_DT", all = TRUE)
-full = merge(x = full, y = sched_scores, by = "MONTH_DT", all = TRUE, suffixes='sched')
+# overall
+
+full = merge(x = qual_scores, y = avail_scores, by = "MONTH_DT", all = TRUE, suffixes=c('_QUAL','_AVAIL'))
+full = merge(x = full, y = sched_scores, by = "MONTH_DT", all = TRUE, suffixes='_SCHED' )
+colnames(full)[4] = 'SCORE_SCHED'
+full = merge(x = full, y = req_scores, by = "MONTH_DT", all = TRUE, suffixes=c('','_REQ') )
+colnames(full)[5] = 'SCORE_REQ'
 
 full$SCORE = apply(full[,seq(2,5)], 1, 
                    function(x) {sum(x,na.rm=TRUE)/sum(!is.na(x))} )
